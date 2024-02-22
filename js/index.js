6dcf8138949908e58db561e9c9989fc62e23efaf
Yuu <phi.phinguyen@setel.com>
2024-02-22 22:18:36 +0700

    fix: update js

--- js/index.js
+++ js/index.js
@@ -1,59 +1,51 @@
-
 for (const [index, p] of document.querySelectorAll("p").entries()) {
     if (p.textContent.search(/!\[\[(.+)excalidraw(.+)\]\]/) !== -1) {
-        console.log(p.textContent)
-        var excalidrawFileWithFormat = p.textContent.match(/!\[\[(.+)\]\]/)[1]
-        var excalidrawFileName = excalidrawFileWithFormat.toString().split('|')[0]
-        var excalidrawFileNameMD = `${excalidrawFileName.trimEnd().trimStart()}.md`;
+        const excalidrawFileWithFormat = p.textContent.match(/!\[\[(.+)\]\]/)[1]
+        const excalidrawFileName = excalidrawFileWithFormat.toString().split('|')[0]
+        const excalidrawFileNameMD = `${excalidrawFileName.trimEnd().trimStart()}.md`;
 
         var newExcalidrawBlock = document.createElement("div");
         newExcalidrawBlock.setAttribute("id", `excalidraw-${index}`);
         newExcalidrawBlock.innerHTML = "";
         p.parentNode.replaceChild(newExcalidrawBlock, p);
 
-        const hahahahahahahtesttt = document.getElementById(`excalidraw-${index}`);
-        console.log('kakkakakakakaakkaakakka')
-        console.log(hahahahahahahtesttt)
-
         let url = `https://raw.githubusercontent.com/0xzphil/blog/main/academy/Excalidraw/${excalidrawFileNameMD}`;
-        setTimeout(() => {
-            fetch(url)
-                .then(res => {
-                    res.text().then(r => {
-                        var code = r.toString().match(/```json(.*)```/s)[1];
-                        var elements = JSON.parse(code).elements;
-                        console.log(elements)
+        fetch(url)
+            .then(res => {
+                res.text().then(r => {
+                    var code = r.toString().match(/```json(.*)```/s)[1];
+                    var elements = JSON.parse(code).elements;
+                    console.log(elements)
 
 
-                        const App = () => {
-                            var options =  {
-                                initialData: {
-                                    elements
-                                }
-                            };
-
-                            return React.createElement(
-                                React.Fragment,
-                                null,
-                                React.createElement(
-                                    "div",
-                                    {
-                                        style: { height: "500px" },
-                                    },
-                                    React.createElement(ExcalidrawLib.Excalidraw, options),
-                                ),
-                            );
+                    const App = () => {
+                        var options =  {
+                            initialData: {
+                                elements
+                            }
                         };
 
-                        const excalidrawWrapper = document.getElementById(`excalidraw-${index}`);
-                        const root = ReactDOM.createRoot(excalidrawWrapper);
-                        root.render(React.createElement(App));
-                    })
+                        return React.createElement(
+                            React.Fragment,
+                            null,
+                            React.createElement(
+                                "div",
+                                {
+                                    style: { height: "500px" },
+                                },
+                                React.createElement(ExcalidrawLib.Excalidraw, options),
+                            ),
+                        );
+                    };
+
+                    const excalidrawWrapper = document.getElementById(`excalidraw-${index}`);
+                    const root = ReactDOM.createRoot(excalidrawWrapper);
+                    root.render(React.createElement(App));
                 })
-                .then(out =>
-                    console.log('Checkout this JSON! ', out))
-                .catch(err => { throw err });
-        }, 20000)
+            })
+            .then(out =>
+                console.log('Checkout this JSON! ', out))
+            .catch(err => { throw err });
 
     }
 }
